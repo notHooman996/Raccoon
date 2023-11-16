c4fe494d892155b68a6c0625b1802588487e3f4d
Ida Marcher Jensen <54858@student.eadania.dk>
2023-11-16 23:43:32 +0100

    InputHandler.cs : Modified * singleton * gets horizontal and vertical axis input * gets interact key input * input works for both keyboard and controller

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,14 +1,59 @@
-using UnityEngine;
+using System;
+using Unity.Collections;
+using UnityEngine; 
 
 public class InputHandler : MonoBehaviour
 {
-    private void Start()
+    // singleton 
+    public static InputHandler Instance; 
+    
+    [Header("MoveControls")] 
+    [SerializeField, ReadOnly] private float horizontal;
+    [SerializeField, ReadOnly] private float vertical;
+
+    [Header("InteractControls")] 
+    [SerializeField, ReadOnly] private bool interact;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this; 
+            
+            // make sure object is not destroyed across scenes 
+            //DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            // enforce singleton, there can only be one instance 
+            Destroy(gameObject);
+        }
     }
 
     private void Update()
     {
-        
+        // set input 
+        SetMovementInput();
+        SetInteractInput();
+    }
+
+    private void SetMovementInput()
+    {
+        (horizontal, vertical) = (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void SetInteractInput()
+    {
+        interact = Input.GetButtonDown("Interact");
+    }
+
+    public (float x, float y) GetMovementInput()
+    {
+        return (horizontal, vertical);
+    }
+
+    public bool GetInteractInput()
+    {
+        return interact;
     }
 }
