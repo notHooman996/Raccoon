5e630b478dc750b90eb73f3b8d1cb95be8aeeefb
Ida Marcher Jensen <54858@student.eadania.dk>
2023-11-18 18:41:19 +0100

    Attributes.cs : Modified * singleton * added property canPlayerMove

--- Assets/Scripts/Attributes.cs
+++ Assets/Scripts/Attributes.cs
@@ -1,14 +1,47 @@
 using UnityEngine;
+using Unity.Collections;
 
 public class Attributes : MonoBehaviour
 {
+    // singleton 
+    public static Attributes Instance;
+
+    [Header("CanPlayerMove")] 
+    [SerializeField, ReadOnly] private bool canPlayerMove;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this; 
+            
+            // make sure object is not destroyed across scenes 
+            //DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            // enforce singleton, there can only be one instance 
+            Destroy(gameObject);
+        }
+    }
+    
     private void Start()
     {
-        
+        canPlayerMove = true; // TODO - set to false when game starts 
     }
 
     private void Update()
     {
         
+    }
+    
+    public void SetCanPlayerMove()
+    {
+        canPlayerMove = !canPlayerMove;
+    }
+    
+    public bool GetCanPlayerMove()
+    {
+        return canPlayerMove; 
     }
 }
