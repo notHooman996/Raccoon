f10edb57d62a091acdb09c51cc2f333610f4a2ae
Ida Marcher Jensen <54858@student.eadania.dk>
2023-11-18 18:46:02 +0100

    TestPauseMenu.cs : Modified * singleton

--- Assets/Scripts/TestPauseMenu.cs
+++ Assets/Scripts/TestPauseMenu.cs
@@ -3,10 +3,28 @@ using System.Collections;
 
 public class TestPauseMenu : MonoBehaviour
 {
+    public static TestPauseMenu Instance; 
+    
     public GameObject pausemenuPrefab; // reference to the canvas ui prefab 
     private CanvasGroup canvasGroup;
     private GameObject canvasInstance; // reference to the instantiated prefab 
     private bool isPaused = false; 
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this; 
+            
+            // make sure object is not destroyed across scenes 
+            //DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            // enforce singleton, there can only be one instance 
+            Destroy(gameObject);
+        }
+    }
     
     private void Start()
     {
@@ -22,7 +40,6 @@ public class TestPauseMenu : MonoBehaviour
             if (isPaused)
             {
                 OpenPauseMenu();
-                
             }
             else
             {
@@ -36,6 +53,7 @@ public class TestPauseMenu : MonoBehaviour
     /// </summary>
     public void OpenPauseMenu()
     {
+        Attributes.Instance.SetCanPlayerMove();
         AddPrefabToScene();
         canvasGroup = canvasInstance.GetComponent<CanvasGroup>();
         StartCoroutine(FadeCanvasGroup(0f, 1f, 2f));
@@ -46,8 +64,14 @@ public class TestPauseMenu : MonoBehaviour
     /// </summary>
     public void ClosePauseMenu()
     {
-        StartCoroutine(FadeCanvasGroup(1f, 0f, 2f));
-        StartCoroutine(FadingOver());
+        isPaused = false; 
+        if (canvasInstance != null)
+        {
+            canvasGroup = canvasInstance.GetComponent<CanvasGroup>();
+            StartCoroutine(FadeCanvasGroup(1f, 0f, 2f));
+            StartCoroutine(FadingOver());
+        }
+        Attributes.Instance.SetCanPlayerMove();
     }
     
     /// <summary>
