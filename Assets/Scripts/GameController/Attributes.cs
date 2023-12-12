55c482e305c33d7e16e204f24fcd6d456b21c7bf
Ida Marcher Jensen <54858@student.eadania.dk>
2023-12-12 20:51:57 +0100

    Attributes.cs : Modified * only has general attributes

--- Assets/Scripts/GameController/Attributes.cs
+++ Assets/Scripts/GameController/Attributes.cs
@@ -2,10 +2,6 @@ using Cinemachine;
 using UnityEngine;
 using Unity.Collections;
 
-public enum MouseHoverType {Ground, Interactable, None}
-public enum InteractableType {Test, Hide}
-public enum CurrentObjective {Move, Interact}
-
 public class Attributes : MonoBehaviour
 {
     // singleton 
@@ -20,15 +16,7 @@ public class Attributes : MonoBehaviour
     [Header("CanPlayerInteract")]
     [SerializeField, ReadOnly] private bool canPlayerInteract = false;
     
-    [Header("MouseHovering")]
-    [SerializeField, ReadOnly] private MouseHoverType mouseHovering;
-    [SerializeField, ReadOnly] private InteractableType mouseInteractableHover;
     
-    [Header("Objective")] 
-    [SerializeField, ReadOnly] private CurrentObjective currentObjective;
-    [SerializeField, ReadOnly] private Vector3 goalPosition;
-    [SerializeField, ReadOnly] private GameObject interactableObject;
-    [SerializeField, ReadOnly] private float interactDistance = 5;
     
     private void Awake()
     {
@@ -84,60 +72,5 @@ public class Attributes : MonoBehaviour
     public bool GetCanPlayerInteract()
     {
         return canPlayerInteract; 
-    }
-
-    public void SetMouseHover(MouseHoverType type)
-    {
-        mouseHovering = type; 
-    }
-
-    public MouseHoverType GetMouseHover()
-    {
-        return mouseHovering; 
-    }
-
-    public void SetMouseInteractableHover(InteractableType type)
-    {
-        mouseInteractableHover = type; 
-    }
-
-    public InteractableType GetMouseInteractableHover()
-    {
-        return mouseInteractableHover; 
-    }
-
-    public void SetCurrentObjective(CurrentObjective objective)
-    {
-        currentObjective = objective; 
-    }
-
-    public CurrentObjective GetCurrentObjective()
-    {
-        return currentObjective; 
-    }
-
-    public void SetGoalPosition(Vector3 position)
-    {
-        goalPosition = position;
-    }
-
-    public Vector3 GetGoalPosition()
-    {
-        return goalPosition; 
-    }
-
-    public void SetInteractable(GameObject obj)
-    {
-        interactableObject = obj; 
-    }
-
-    public GameObject GetInteractable()
-    {
-        return interactableObject; 
-    }
-
-    public float GetInteractDistance()
-    {
-        return interactDistance; 
     }
 }
