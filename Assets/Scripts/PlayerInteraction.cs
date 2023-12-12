5f4c59ce1224379815d432c2fb14fb7ed2c3fe05
Ida Marcher Jensen <54858@student.eadania.dk>
2023-12-12 19:24:40 +0100

    PlayerInteraction.cs : Modified * set code into method KeyboardControllerInteraction, for handling interaction when playing with keyboard or controller. * added method for interaction for point and click interaction.

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -9,11 +9,30 @@ public class PlayerInteraction : MonoBehaviour
 
     private void Update()
     {
+        KeyboardControllerInteraction();
+        PointAndClickInteraction();
+    }
+
+    private void KeyboardControllerInteraction()
+    {
         if (Attributes.Instance.GetCanPlayerInteract())
         {
             if (InputHandler.Instance.GetInteractInput())
             {
-                Debug.Log("interact");
+                Debug.Log("interact - keyboard, controller");
+            }
+        }
+    }
+
+    private void PointAndClickInteraction()
+    {
+        if (Attributes.Instance.GetCurrentObjective() == CurrentObjective.Interact)
+        {
+            // check if player is close enough to the chosen interactable object 
+            float distance = Vector3.Distance(GameObject.FindWithTag("Player").transform.position, Attributes.Instance.GetInteractable().transform.position);
+            if (distance <= Attributes.Instance.GetInteractDistance())
+            {
+                Debug.Log("interact - point and click");
             }
         }
     }
