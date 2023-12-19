c0f957e69a5774f8256707fed16753d802cee691
Ida Marcher Jensen <54858@student.eadania.dk>
2023-12-19 23:11:52 +0100

    GraphGenerator.cs : Modified

--- Assets/Scripts/Pathfinding/GraphEditor/GraphGenerator.cs
+++ Assets/Scripts/Pathfinding/GraphEditor/GraphGenerator.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GraphGenerator
@@ -22,33 +23,97 @@ public class GraphGenerator
         walkableObjects = walkables;
         colliderObjects = colliders;
 
-        GenerateVertices();
+        //GenerateVertices();
     }
     
     private static void GenerateVertices()
     {
         foreach (GameObject colliderObject in colliderObjects)
         {
-            // get the meshe of the collider object 
-            Mesh mesh = colliderObject.GetComponent<Mesh>();
-            
-            // get the corners of the mesh 
-            
+            // get the mesh filter component from the object 
+            MeshFilter meshFilter = colliderObject.GetComponent<MeshFilter>();
+
+            if (meshFilter != null)
+            {
+                // get the mesh of the collider object 
+                Mesh mesh = meshFilter.mesh; 
+
+                // get the corners of the mesh 
+                if (mesh != null)
+                {
+                    // get the mesh vertices 
+                    Vector3[] meshVertices = mesh.vertices;
+
+                    // get the transom of the object 
+                    Transform objTransform = colliderObject.transform;
+
+                    // loop through each mesh vertex to get the worldspace position 
+                    for (int i = 0; i < meshVertices.Length; i++)
+                    {
+                        // transform of the mesh vertex from local space to world space 
+                        Vector3 cornerWorldPosition = objTransform.TransformPoint(meshVertices[i]);
+
+                        // check where mesh vertex intersects with walkables 
+                        foreach (GameObject walkableObject in walkableObjects)
+                        {
+                            // get the walkable object as a plane 
+                            Plane plane = new Plane(walkableObject.transform.up, walkableObject.transform.position);
+                            
+                            // create ray from the mesh vertex along the normal towards the walkable 
+                            Ray ray = new Ray(cornerWorldPosition, -plane.normal);
+
+                            float enter; 
+                            // check for intersection between ray and the walkable 
+                            if (plane.Raycast(ray, out enter))
+                            {
+                                // get the intersection point 
+                                Vector3 intersectionPoint = ray.GetPoint(enter);
+                                
+                                // Output the intersection point
+                                Debug.Log("Intersection point for corner " + i + ": " + intersectionPoint);
+                                vertices.Add(new Vertex("A", intersectionPoint));
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Mesh not found.");
+                }
+            }
         }
-        
-        
-        
-        
-        foreach (GameObject walkable in walkableObjects)
+    }
+
+    public static void CreateVertices()
+    {
+        Debug.Log("test1");
+        // check for mouse left click 
+        if (Event.current.type == EventType.MouseDown && Event.current.button == 0) 
         {
-            // get the position 
-            Vector3 objectPosition = walkable.transform.position; 
+            Debug.Log("test2");
+            
+            // cast a ray from the mouse position in relation to the camera's view 
+            Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+            RaycastHit hitInfo;
 
-            // get the scale in each direction 
-            Vector3 objectScale = walkable.transform.localScale; 
-
-            // add vertex 
-            vertices.Add(new Vertex("A", objectPosition));
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                // check if ray hits objects in walkable list 
+                foreach (GameObject gameObject in walkableObjects)
+                {
+                    if (hitInfo.collider.gameObject == gameObject)
+                    {
+                        Debug.Log("Hit object: " + gameObject.name);
+                        
+                        // create vertex at entry point 
+                    }
+                }
+            }
         }
+    }
+
+    private static void CreateVertex()
+    {
+        
     }
 }
