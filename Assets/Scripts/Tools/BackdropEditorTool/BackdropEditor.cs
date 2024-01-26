9ac2c0b6b4861bb6e7262f4bf87a518d6908eb5a
Ida Marcher Jensen <54858@student.eadania.dk>
2024-01-27 00:36:43 +0100

    BackdropEditor.cs : Modified * window tab for backdrop editing tool. * edits the selected backdrop.

---
+++ Assets/Scripts/Tools/BackdropEditorTool/BackdropEditor.cs
@@ -0,0 +1,98 @@
+using UnityEditor;
+using UnityEngine;
+
+public class BackdropEditor : EditorWindow
+{
+    private bool showEditBackdropTransform = false;
+
+    public void DrawEditTab()
+    {
+        EditMenu();
+    }
+    
+    private void EditMenu()
+    {
+        EditorGUILayout.LabelField("Edit backdrop", EditorStyles.boldLabel);
+
+        if (BackdropSelect.SelectedBackdrop == null)
+        {
+            GUILayout.Label("Choose backdrop from list to edit.");
+        }
+        else
+        {
+            if (GUILayout.Button("Focus Backdrop", GUILayout.MaxWidth(120)))
+            {
+                SceneView sceneView = SceneView.lastActiveSceneView;
+                if (sceneView != null)
+                {
+                    // Set the scene view pivot to the position of the selected object
+                    sceneView.LookAt(BackdropSelect.SelectedBackdrop.transform.position, sceneView.rotation);
+                    
+                    // repaint the scene view to reflect the changes 
+                    sceneView.Repaint();
+                }
+            }
+
+            EditBackdrop();
+            EditFloor();
+            EditLayer();
+        }
+    }
+
+    private void EditBackdrop()
+    {
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.ObjectField("Name: "+BackdropSelect.SelectedBackdrop.name, BackdropSelect.SelectedBackdrop, typeof(GameObject), true);
+        EditorGUI.EndDisabledGroup();
+        
+        ChangeName(BackdropSelect.SelectedBackdrop);
+        Transform(ref showEditBackdropTransform, BackdropSelect.SelectedBackdrop);
+    }
+
+    private void ChangeName(GameObject gameObject)
+    {
+        // change name 
+        string newName = gameObject.name; 
+        newName = EditorGUILayout.TextField("Rename: ", newName);
+        gameObject.name = newName;
+    }
+
+    private void Transform(ref bool showTransform, GameObject gameObject)
+    {
+        showTransform = EditorGUILayout.Foldout(showTransform, "Transform");
+        if (showTransform)
+        {
+            EditorGUI.BeginChangeCheck();
+            
+            // Display position fields
+            gameObject.transform.position = EditorGUILayout.Vector3Field("Position", gameObject.transform.position);
+            
+            // Display rotation fields 
+            gameObject.transform.rotation = Quaternion.Euler(EditorGUILayout.Vector3Field("Rotation", gameObject.transform.rotation.eulerAngles));
+            
+            // Display scale fields
+            gameObject.transform.localScale = EditorGUILayout.Vector3Field("Scale", gameObject.transform.localScale);
+            
+            // Check if any changes were made
+            if (EditorGUI.EndChangeCheck())
+            {
+                // Mark the object as dirty to apply the changes
+                EditorUtility.SetDirty(gameObject);
+            }
+        }
+    }
+
+    private void EditFloor()
+    {
+        
+    }
+
+    private void EditLayer()
+    {
+        // edit number of layers (give the layers ids)
+        // edit width between each layer 
+
+        // add elements to a layer 
+            // choose layer first 
+    }
+}
