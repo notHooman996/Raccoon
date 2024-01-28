9fa5accfa05d90af421e7db6401414c0fdbccf0e
Ida Marcher Jensen <54858@student.eadania.dk>
2024-01-28 20:58:25 +0100

    BackdropLayer.cs : Modified * let the id and spacing be visible, readonly, in the inspector window when selecting a layer in the scene.

--- Assets/Scripts/Tools/BackdropEditorTool/BackdropLayer.cs
+++ Assets/Scripts/Tools/BackdropEditorTool/BackdropLayer.cs
@@ -1,20 +1,27 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BackdropLayer : MonoBehaviour
 {
-    [SerializeField] private int id;
-    [SerializeField] private float widthBetween; 
+    [SerializeField] private int layerID;
+    [SerializeField] private float layerSpacing;
+    
+    public int LayerID
+    {
+        get { return layerID; }
+        set { layerID = value; }
+    }
+    
+    public float LayerSpacing
+    {
+        get { return layerSpacing; }
+        set { layerSpacing = value; }
+    }
     
     // list of trees, rocks, bushes on the layer 
     
-    private void Start()
-    {
-        
-    }
-
-    private void Update()
-    {
-        
-    }
+    
+    // create method that positions the layer correctly in relation to the backdrop 
+    
+    
+    
 }
