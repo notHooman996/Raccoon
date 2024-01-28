a1a4fb77cd9df302fe473851a1c7e7b3802ad4c5
Ida Marcher Jensen <54858@student.eadania.dk>
2024-01-28 20:56:06 +0100

    BackdropLoad.cs : Modified * load the prefabs for backdrop and layer

--- Assets/Scripts/Tools/BackdropEditorTool/BackdropLoad.cs
+++ Assets/Scripts/Tools/BackdropEditorTool/BackdropLoad.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class BackdropLoad : EditorWindow
 {
-    public static GameObject BackdropHolderObject { get; set; }
+    private string backdropPrefabPath = "Assets/Prefabs/TheatreBackdrop/Backdrop.prefab";
+    private string layerPrefabPath = "Assets/Prefabs/TheatreBackdrop/Layer.prefab";
     
-    public void LoadStage()
+    public static GameObject BackdropHolderObject { get; set; }
+    public static GameObject BackdropPrefab { get; private set; }
+    public static GameObject LayerPrefab { get; private set; }
+
+    public void Load()
+    {
+        LoadStage();
+        LoadPrefabs();
+    }
+    
+    private void LoadStage()
     {
         // load backdrop holder 
         BackdropHolderObject = GameObject.FindGameObjectWithTag("BackdropHolder");
         
         // load the backdrops 
         BackdropSelect.Backdrops = GameObject.FindGameObjectsWithTag("Backdrop").ToList();
+    }
+
+    private void LoadPrefabs()
+    {
+        BackdropPrefab = AssetDatabase.LoadAssetAtPath(backdropPrefabPath, typeof(GameObject)) as GameObject;
+        LayerPrefab = AssetDatabase.LoadAssetAtPath(layerPrefabPath, typeof(GameObject)) as GameObject;
     }
 }
