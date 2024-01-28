357ed7045815d3d3fa2cb742ba98c189282feb6b
Ida Marcher Jensen <54858@student.eadania.dk>
2024-01-28 21:01:31 +0100

    BackdropCreater.cs : Modified * loading the prefabs happens in BackdropLoad.cs * added method AddLayer, that adds a new layer to the selected backdrop.

--- Assets/Scripts/Tools/BackdropEditorTool/BackdropCreater.cs
+++ Assets/Scripts/Tools/BackdropEditorTool/BackdropCreater.cs
@@ -4,43 +4,23 @@ using UnityEngine;
 public class BackdropCreater : EditorWindow
 {
     private Vector2 scrollPosition;
-    [SerializeField] private GameObject backdropPrefab; 
 
     public void DrawCreateTab()
     {
         scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
         
-        DrawHorizontalLine();
+        BackdropTool.DrawHorizontalLine();
         AddBackdrop();
-        DrawHorizontalLine();
-        DrawHorizontalLine();
+        BackdropTool.DrawHorizontalLine();
+        AddLayer();
+        BackdropTool.DrawHorizontalLine();
         
         EditorGUILayout.EndScrollView();
-    }
-    
-    private void DrawHorizontalLine()
-    {
-        GUILayout.Space(10);
-        
-        Color color = Color.gray;
-        int thickness = 1;
-        int padding = 10; 
-        
-        Rect rect = EditorGUILayout.GetControlRect(false, thickness, EditorStyles.helpBox);
-        rect.height = thickness;
-        rect.y += padding / 2;
-        rect.x -= 2;
-        rect.width += 6;
-        EditorGUI.DrawRect(rect, color);
-        
-        GUILayout.Space(10);
     }
     
     private void AddBackdrop()
     {
         EditorGUILayout.LabelField("Add backdrop", EditorStyles.boldLabel);
-        
-        SetBackdropPrefab();
         
         // use prefab 
         if (GUILayout.Button("Add Backdrop"))
@@ -54,15 +34,30 @@ public class BackdropCreater : EditorWindow
             }
             
             // new backdrop gameobject 
-            GameObject newBackdrop = PrefabUtility.InstantiatePrefab(backdropPrefab) as GameObject;
+            GameObject newBackdrop = PrefabUtility.InstantiatePrefab(BackdropLoad.BackdropPrefab) as GameObject;
             newBackdrop.transform.parent = BackdropLoad.BackdropHolderObject.transform; 
             BackdropSelect.Backdrops.Add(newBackdrop);
         }
     }
     
-    private void SetBackdropPrefab()
+    private void AddLayer()
     {
-        // Display the stored prefab reference 
-        backdropPrefab = (GameObject)EditorGUILayout.ObjectField("BackdropPrefab", backdropPrefab, typeof(GameObject), false);
+        EditorGUILayout.LabelField("Add layer", EditorStyles.boldLabel);
+        
+        if (BackdropSelect.SelectedBackdrop == null)
+        {
+            GUILayout.Label("Choose backdrop from list.");
+        }
+        else
+        {
+            // use prefab 
+            if (GUILayout.Button("Add Layer"))
+            {
+                // new layer gameobject 
+                GameObject newLayer = PrefabUtility.InstantiatePrefab(BackdropLoad.LayerPrefab) as GameObject;
+                newLayer.transform.parent = BackdropSelect.SelectedBackdrop.transform; 
+                BackdropSelect.SelectedBackdrop.GetComponent<Backdrop>().AddLayer(newLayer);
+            }
+        }
     }
 }
