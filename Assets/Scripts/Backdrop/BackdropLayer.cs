75bfb2428df1fd1cad573dd79fa8f3f5e08f7b6d
Ida Marcher Jensen <54858@student.eadania.dk>
2024-02-04 23:07:53 +0100

    BackdropLayer.cs : Modified * sets the sprites position and rotation

--- Assets/Scripts/Backdrop/BackdropLayer.cs
+++ Assets/Scripts/Backdrop/BackdropLayer.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BackdropLayer : MonoBehaviour
@@ -17,11 +19,42 @@ public class BackdropLayer : MonoBehaviour
         set { layerSpacing = value; }
     }
     
-    // list of trees, rocks, bushes on the layer 
+    public List<GameObject> Sprites { get; private set; }
     
+    // method is called when script is loaded 
+    private void OnValidate()
+    {
+        LoadSprites();
+        DrawSprites();
+    }
     
-    // create method that positions the layer correctly in relation to the backdrop 
-    
-    
-    
+    private void LoadSprites()
+    {
+        Sprites = transform
+            .GetComponentsInChildren<Transform>(true)
+            .Where(child => child.CompareTag("Sprite"))
+            .Select(child => child.gameObject)
+            .ToList();
+    }
+
+    public void AddSprite(GameObject spriteObject)
+    {
+        // set sprite objects position relative to layer 
+        spriteObject.transform.localPosition = new Vector3(spriteObject.transform.localPosition.x, spriteObject.transform.localPosition.y, 0f);
+        // set sprite objects rotation to match the layers rotation 
+        spriteObject.transform.localRotation = transform.localRotation; 
+        
+        Sprites.Add(spriteObject);
+    }
+
+    public void DrawSprites()
+    {
+        for (int i = 0; i < Sprites.Count; i++)
+        {
+            // set sprite objects position relative to layer 
+            Sprites[i].transform.localPosition = new Vector3(Sprites[i].transform.localPosition.x, Sprites[i].transform.localPosition.y, 0f);
+            // set sprite objects rotation to match the layers rotation 
+            Sprites[i].transform.localRotation = transform.localRotation; 
+        }
+    }
 }
