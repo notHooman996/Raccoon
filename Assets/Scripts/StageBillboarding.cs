9eed9faa53abd6c8dcddeadbaa73a82069d44192
Ida Marcher Jensen <54858@student.eadania.dk>
2024-02-09 23:58:55 +0100

    StageBillboarding.cs : Added * script attached to Stage gameobjects. Used for handling billboarding on sprites.

---
+++ Assets/Scripts/StageBillboarding.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class StageBillboarding : MonoBehaviour
+{
+    private List<GameObject> sprites = new List<GameObject>();
+    
+    public bool DoStageBillboarding { get; set; }
+    
+    private void Start()
+    {
+        FindChildObjectsWithTag(transform, "Sprite");
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void SetBillboarding()
+    {
+        // set all sprite objects in the stage to do or not do billboarding 
+        foreach (GameObject sprite in sprites)
+        {
+            sprite.GetComponent<Billboarding>().DoStageBillboarding = DoStageBillboarding; 
+        }
+    }
+
+    private void FindChildObjectsWithTag(Transform parent, string tag)
+    {
+        foreach (Transform child in parent)
+        {
+            // if the child has a specified tag, add it to the list 
+            if (child.CompareTag(tag))
+            {
+                sprites.Add(child.gameObject);
+            }
+            
+            // recursively search through children 
+            if (child.childCount > 0)
+            {
+                FindChildObjectsWithTag(child, tag);
+            }
+        }
+    }
+}
