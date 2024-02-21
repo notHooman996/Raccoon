636fa00081bc37482d7c82a225c36189a5f99714
Ida Marcher Jensen <54858@student.eadania.dk>
2024-02-21 16:56:52 +0100

    AttributesPlayer.cs : Modified * added variables: canPlayerMove, canPlayerInteract.

--- Assets/Scripts/GameController/AttributesPlayer.cs
+++ Assets/Scripts/GameController/AttributesPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.Collections;
 using UnityEngine;
 
@@ -25,10 +26,33 @@ namespace GameController
 
         [Header("ActiveVirtualCamera")] 
         [SerializeField, ReadOnly] private float playerSpeed = 5f;
+        
+        [Header("CanPlayerMove")] 
+        [SerializeField, ReadOnly] private bool canPlayerMove;
+    
+        [Header("CanPlayerInteract")]
+        [SerializeField, ReadOnly] private bool canPlayerInteract = false; 
+
+        private void Start()
+        {
+            canPlayerMove = true; // TODO - set to false when game starts 
+        }
 
         public float GetPlayerSpeed()
         {
             return playerSpeed; 
+        }
+        
+        public bool CanPlayerMove
+        {
+            get { return canPlayerMove; }
+            set { canPlayerMove = value; }
+        }
+
+        public bool CanPlayerInteract
+        {
+            get { return canPlayerInteract; }
+            set { canPlayerInteract = value; }
         }
     }
 }
