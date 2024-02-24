0fb7b3a7fb1b9e8f6f0a330602ff1d3353701f3e
Ida Marcher Jensen <54858@student.eadania.dk>
2024-02-24 11:15:10 +0100

    Attributes.cs : Modified * use for something else, maybe.

--- Assets/Scripts/GameController/Attributes.cs
+++ Assets/Scripts/GameController/Attributes.cs
@@ -15,32 +15,12 @@ public class Attributes : MonoBehaviour
             Instance = this; 
             
             // make sure object is not destroyed across scenes 
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         else if (Instance != this)
         {
             // enforce singleton, there can only be one instance 
             Destroy(gameObject);
         }
-    }
-    
-    [Header("List of Scenes")]
-    [SerializeField, ReadOnly] private List<Scene> scenes = new List<Scene>();
-
-    private int currentSceneIndex = 0; 
-    
-    private void Start()
-    {
-        // canPlayerMove = true; // TODO - set to false when game starts 
-    }
-
-    private void Update()
-    {
-        
-    }
-
-    public Scene CurrentScene
-    {
-        get { return scenes[currentSceneIndex]; }
     }
 }
