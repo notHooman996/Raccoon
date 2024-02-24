e9b2b167d07cad185867a7c9c262b72290a5e3f4
Ida Marcher Jensen <54858@student.eadania.dk>
2024-02-24 16:09:03 +0100

    GameManager.cs : Modified * made it a singleton. * added getter for current scene index. * added method RestartGame, for using when creating new game from the main menu.

--- Assets/Scripts/GameController/GameManager.cs
+++ Assets/Scripts/GameController/GameManager.cs
@@ -1,13 +1,42 @@
-using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // singleton 
+    public static GameManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this; 
+            
+            // make sure object is not destroyed across scenes 
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            // enforce singleton, there can only be one instance 
+            Destroy(gameObject);
+        }
+    }
+    
     private GameData gameData;
+
+    private int sceneIndex;
 
     private void Start()
     {
-        Debug.Log("Hello");
-        int sceneIndex = gameData.sceneIndex; 
+        sceneIndex = gameData.sceneIndex; 
+    }
+
+    public int GetCurrentSceneIndex()
+    {
+        return sceneIndex; 
+    }
+
+    public void RestartGame()
+    {
+        sceneIndex = 0;
+        gameData.sceneIndex = sceneIndex; 
     }
 }
