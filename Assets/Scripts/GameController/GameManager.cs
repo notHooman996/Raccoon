74daa9b0c451f1b2a54be3bea78a0375d98ece39
Ida Marcher Jensen <54858@student.eadania.dk>
2024-02-24 12:22:11 +0100

    GameManager.cs : Added * script for managing save game data.

---
+++ Assets/Scripts/GameController/GameManager.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameData gameData;
+
+    private void Start()
+    {
+        int sceneIndex = gameData.sceneIndex; 
+    }
+}
