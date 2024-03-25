827b2b48caf1e203509b2d5364d08ef3c87974df
Ida Marcher Jensen <54858@student.eadania.dk>
2024-03-25 19:34:38 +0100

    AttributesPlayer.cs : Modified * added interactDistance as an attribute, along with a getter

--- Assets/Scripts/GameController/AttributesPlayer.cs
+++ Assets/Scripts/GameController/AttributesPlayer.cs
@@ -2,57 +2,62 @@ using System;
 using Unity.Collections;
 using UnityEngine;
 
-namespace GameController
+public class AttributesPlayer : MonoBehaviour
 {
-    public class AttributesPlayer : MonoBehaviour
+    // singleton 
+    public static AttributesPlayer Instance;
+    private void Awake()
     {
-        // singleton 
-        public static AttributesPlayer Instance;
-        private void Awake()
+        if (Instance == null)
         {
-            if (Instance == null)
-            {
-                Instance = this; 
-            
-                // make sure object is not destroyed across scenes 
-                //DontDestroyOnLoad(gameObject);
-            }
-            else if (Instance != this)
-            {
-                // enforce singleton, there can only be one instance 
-                Destroy(gameObject);
-            }
-        }
-
-        [Header("ActiveVirtualCamera")] 
-        [SerializeField, ReadOnly] private float playerSpeed = 5f;
+            Instance = this; 
         
-        [Header("CanPlayerMove")] 
-        [SerializeField, ReadOnly] private bool canPlayerMove;
+            // make sure object is not destroyed across scenes 
+            //DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            // enforce singleton, there can only be one instance 
+            Destroy(gameObject);
+        }
+    }
+
+    [Header("ActiveVirtualCamera")] 
+    [SerializeField, ReadOnly] private float playerSpeed = 5f;
     
-        [Header("CanPlayerInteract")]
-        [SerializeField, ReadOnly] private bool canPlayerInteract = false; 
+    [Header("CanPlayerMove")] 
+    [SerializeField, ReadOnly] private bool canPlayerMove;
 
-        private void Start()
-        {
-            canPlayerMove = true; // TODO - set to false when game starts 
-        }
+    [Header("CanPlayerInteract")]
+    [SerializeField, ReadOnly] private bool canPlayerInteract = false; 
+    
+    [Header("PlayerInteractDistance")]
+    [SerializeField, ReadOnly] private float interactDistance = 3; 
 
-        public float GetPlayerSpeed()
-        {
-            return playerSpeed; 
-        }
-        
-        public bool CanPlayerMove
-        {
-            get { return canPlayerMove; }
-            set { canPlayerMove = value; }
-        }
+    private void Start()
+    {
+        canPlayerMove = true; // TODO - set to false when game starts 
+    }
 
-        public bool CanPlayerInteract
-        {
-            get { return canPlayerInteract; }
-            set { canPlayerInteract = value; }
-        }
+    public float GetPlayerSpeed()
+    {
+        return playerSpeed; 
+    }
+    
+    public bool CanPlayerMove
+    {
+        get { return canPlayerMove; }
+        set { canPlayerMove = value; }
+    }
+
+    public bool CanPlayerInteract
+    {
+        get { return canPlayerInteract; }
+        set { canPlayerInteract = value; }
+    }
+    
+    public float InteractDistance
+    {
+        get { return interactDistance; }
     }
 }
